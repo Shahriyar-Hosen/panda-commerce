08701c26b7e3b55fd607be087f3492f343ffdfc0
Salman <you@example.com>
2022-02-16 11:01:36 +0600

    js comment

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
-const h2Color = document.getElementsByTagName('h2');
-for (const hheading2 of h2Color) {
-    hheading2.style.color = 'lightblue';
-}
+// const h2Color = document.getElementsByTagName('h2');
+// for (const hheading2 of h2Color) {
+//     hheading2.style.color = 'lightblue';
+// }
 
-document.getElementById('backpack').style.backgroundColor = 'tomato' 
+// document.getElementById('backpack').style.backgroundColor = 'tomato' 
 
-const cardBorder = document.getElementsByClassName('card ');
-for (const card of cardBorder) {
-    card.style.borderRadius = '30px';
-}
+// const cardBorder = document.getElementsByClassName('card ');
+// for (const card of cardBorder) {
+//     card.style.borderRadius = '30px';
+// }
 
-document.getElementById('buy-now-btn').addEventListener('click',function () {
-    console.log("buy-now-btn")
-})
+// document.getElementById('buy-now-btn').addEventListener('click',function () {
+//     console.log("buy-now-btn")
+// })
 
-document.getElementById('remove-btn').addEventListener('click', function (e) {
-    if (e.target.innerText == 'Buy Now') {
-        e.target.parentNode.removeChild(e.target);
-    }
-})
+// document.getElementById('remove-btn').addEventListener('click', function (e) {
+//     if (e.target.innerText == 'Buy Now') {
+//         e.target.parentNode.removeChild(e.target);
+//     }
+// })
 
-document.getElementById('submit-input').addEventListener('keyup', function (e) {
-    const submitBtn = document.getElementById('submit-btn');
+// document.getElementById('submit-input').addEventListener('keyup', function (e) {
+//     const submitBtn = document.getElementById('submit-btn');
 
-    if (e.target.value == "email") {
-        submitBtn.removeAttribute('disabled')
-    }
-    else {
-        submitBtn.setAttribute('disabled', true)
-    }
-})
+//     if (e.target.value == "email") {
+//         submitBtn.removeAttribute('disabled')
+//     }
+//     else {
+//         submitBtn.setAttribute('disabled', true)
+//     }
+// })
